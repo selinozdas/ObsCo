56c8c3074836881de4d198dc01d772e364b0accb
Doğan Can Eren <dgncnrn@gmail.com>
2019-04-21 12:42:57 +0300

    Create Main.java

---
+++ code/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Main http = new Main();
+        try{
+            System.out.println("Testing 1 - Send Http GET request");
+            http.sendGet();
+
+        } catch (Exception e) {
+            System.err.println("Oops!");
+            e.printStackTrace();
+        }
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://127.0.0.1:5000/obsco/api/v1.0/users";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        //con.setRequestProperty("User-Agent",);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+}
