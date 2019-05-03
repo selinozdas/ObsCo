e95bc0652528f90419a7c10d40e46588bb24481c
Doğan Can Eren <dgncnrn@gmail.com>
2019-05-03 20:30:08 +0300

    obsco.py db integration

--- code/obsco.py
+++ code/obsco.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, abort, make_response, request
 #import Accountant, Analyzer, DBManager, Group, ObsCoManager, User, Variables
+#from DBManager import DBManager
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/obsco"
+mongo = PyMongo(app)
 
 users = [
     {
@@ -69,6 +73,22 @@ users = [
     }
 ]
 
+groups = [
+    {
+            "id":1,
+            "name":"Instructors",
+            "members":[12345671,12345672,12345673]
+    },
+    {
+            "id":2,
+            "name":"Orchestrion",
+            "members":[21400527,21400528,21400529,21400530,21400531]
+    }
+]
+
+#man = DBManager()
+#val = man.fetchUser(userId=12345671)
+
 @app.route('/')
 def hello_world():
     return 'ObsCo initialized'
@@ -84,9 +104,26 @@ def get_user(user_id):
         abort(404)
     return jsonify({'user': user[0]})
 
+@app.route('/obsco/api/v1.0/groups', methods=['GET'])
+def get_groups():
+    return jsonify({'groups': groups})
+
+@app.route('/obsco/api/v1.0/groups/<int:group_id>', methods=['GET'])
+def get_group(group_id):
+    group = [group for group in groups if group['id'] == group_id]
+    if len(group) == 0:
+        abort(404)
+    return jsonify({'group': group[0]})
+
+@app.route('/obsco/api/v1.0/dbusers', methods=['GET'])
+def get_dbusers():
+    online_users = mongo.db.users.find({"id": 12345671},{'_id':0})
+    x = [i for i in online_users]
+    return jsonify({"index.html": x})
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
