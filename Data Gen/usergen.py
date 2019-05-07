0105e1235e6afeb7492040fe72e83b976fd13282
cerenuysal <ceren.uysal@ug.bilkent.edu.tr>
2019-05-07 17:56:22 +0300

    Update usergen.py

--- Data Gen/usergen.py
+++ Data Gen/usergen.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 #change url
-mongo_url = 'mongodb://127.0.0.1:29298/'
+mongo_url = 'mongodb://127.0.0.1:27017/'
 client = MongoClient(mongo_url)
 
 db = client["obsco"]
@@ -479,6 +479,203 @@ db.users.insert_many(
              "value": 5
             }
         ]
+    },
+    {   "name" : "Tanay Akgul",
+        "id": 21400527,
+        "groups" : [
+            {'id':2,
+            'members':1,
+            'owner':0,
+            'leaders':1},
+            {'id':7,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':8,
+            'members':1,
+            'owner':1,
+            'leaders':1}
+        ],
+        "email" : "tanay.akgul@ug.bilkent.edu.tr",
+        "password" : "betterthanever",
+        "age" : 24,
+        "superuser" : False,
+        "title" : "Student",
+        "skills":[{
+             "id": 2,
+             "value": 5
+            },
+            {
+             "id": 3,
+             "value": 8
+            },
+            {
+             "id": 12,
+             "value": 6
+            }
+        ]
+    },
+    {   "name" : "Furkan Bacak",
+        "id": 21400528,
+        "groups" : [
+            {'id':2,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':7,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':8,
+            'members':0,
+            'owner':0,
+            'leaders':1},
+            {'id':9,
+            'members':1,
+            'owner':1,
+            'leaders':1}
+        ],
+        "email" : "furkan.bacak@ug.bilkent.edu.tr",
+        "password" : "bilemiyorumaltan",
+        "age" : 24,
+        "superuser" : False,
+        "title" : "Student",
+        "skills":[{
+             "id": 2,
+             "value": 4
+            },
+            {
+             "id": 7,
+             "value": 8
+            },
+            {
+             "id": 8,
+             "value": 5
+            },
+            {
+             "id": 13,
+             "value": 6
+            },
+            {
+             "id": 14,
+             "value": 8
+            }
+        ]
+
+    },
+    {   "name" : "Pinar Bayata",
+        "id": 21400529,
+        "groups" : [
+            {'id':2,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':7,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':8,
+            'members':1,
+            'owner':0,
+            'leaders':0}
+        ],
+        "email" : "pinar.bayata@ug.bilkent.edu.tr",
+        "password" : "makeup",
+        "age" : 24,
+        "superuser" : False,
+        "title" : "Student",
+        "skills":[{
+             "id": 3,
+             "value": 2
+            },
+            {
+             "id": 8,
+             "value": 7
+            },
+            {
+             "id": 10,
+             "value": 5
+            },
+            {
+             "id": 13,
+             "value": 1
+            }
+        ]
+
+    },
+    {   "name" : "Omer Faruk Ergun",
+        "id": 21400530,
+        "groups" : [
+            {'id':2,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':7,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':9,
+            'members':1,
+            'owner':0,
+            'leaders':0}
+        ],
+        "email" : "faruk.ergun@ug.bilkent.edu.tr",
+        "password" : "faruk96",
+        "age" : 24,
+        "superuser" : False,
+        "title" : "Student",
+        "skills":[{
+             "id": 9,
+             "value": 8
+            },
+            {
+             "id": 11,
+             "value": 2
+            },
+            {
+             "id": 14,
+             "value": 3
+            }
+        ]
+
+    },
+    {   "name" : "Burak Sibirlioglu",
+        "id": 21400531,
+        "groups" : [
+            {'id':2,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':7,
+            'members':1,
+            'owner':0,
+            'leaders':0},
+            {'id':9,
+            'members':1,
+            'owner':0,
+            'leaders':0}
+        ],
+        "email" : "burak.sibirlioglu@ug.bilkent.edu.tr",
+        "password" : "codingiseverything",
+        "age" : 23,
+        "superuser" : False,
+        "title" : "Student",
+        "skills":[{
+             "id": 10,
+             "value": 8
+            },
+            {
+             "id": 11,
+             "value": 4
+            },
+            {
+             "id": 14,
+             "value": 6
+            }
+
+        ]
+
     }
+
 ]
 )
